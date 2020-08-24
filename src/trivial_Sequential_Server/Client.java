754e07fccae6fa10794466d4ea94342ab069ed53
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-08-24 13:38:20 +0200

    Client

--- src/trivial_Sequential_Server/Client.java
+++ src/trivial_Sequential_Server/Client.java
@@ -1,4 +1,14 @@
 package trivial_Sequential_Server;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        var socket = new Socket("192.168.43.73", 8000);
+        var in = new Scanner(socket.getInputStream());
+        var input = in.nextLine();
+        System.out.println("Server response: " + input);
+    }
 }
